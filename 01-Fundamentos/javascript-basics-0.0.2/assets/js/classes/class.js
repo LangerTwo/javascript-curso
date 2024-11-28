030e2a2f36ed2bf3d6e47e255c98a5cd90899c6a
LangerTwo <adrangel95@gmail.com>
2024-11-28 14:54:18 -0500

    set y gets

--- 01-Fundamentos/javascript-basics-0.0.2/assets/js/classes/class.js
+++ 01-Fundamentos/javascript-basics-0.0.2/assets/js/classes/class.js
@@ -4,11 +4,19 @@ class Persona {
     nombre = '';
     codigo = '';
     frase = '';
+    comida = '';
 
     constructor(nombre = 'Sin nombre', codigo = 'Sin código', frase = 'Sin frase') {
         this.nombre = nombre;
         this.codigo = codigo;
         this.frase = frase;
+    }
+
+    set setComidadFavorita( comida ) {
+        this.comida = comida.toUpperCase();
+    }
+    get getComidaFavorita() {
+        return `La comida favorita de ${ this.nombre } es ${ this.comida }`
     }
 
     quienSoy() {
@@ -24,11 +32,13 @@ class Persona {
 const spiderman = new Persona('Peter Parker', 'Spiderman', 'Soy tu amigable vecino spiderman');
 const ironman = new Persona('Tony Stark', 'Ironman', 'Yo soy Ironman');
 
-// console.log( spiderman )
-// console.log( ironman )
 
 // spiderman.quienSoy();
+console.log( spiderman )
 spiderman.miFrase();
+spiderman.setComidadFavorita = 'El pie de cereza de la tía May';
+console.log( spiderman.getComidaFavorita )
 
+// console.log( ironman )
 // ironman.quienSoy();
 // ironman.miFrase();
