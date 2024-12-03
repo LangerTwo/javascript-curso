196f18be4ec36c8c532cd7887b1d587a89ab6cfe
LangerTwo <adrangel95@gmail.com>
2024-12-03 15:09:54 -0500

    blackjack vite

---
+++ 03-vite-app/src/blackjack/usecases/crear-deck.js
@@ -0,0 +1,32 @@
+import _ from 'underscore';
+
+/**
+ * Esta Función crea un nuevo deck
+ * @param {Array<string>} tiposDeCarta Ejemplo: ['C','D','H','S']
+ * @param {Array<string>} tiposEspeciales Ejemplo: ['A','J','Q','K']
+ * @returns {Array} retorna un nuevo deck e cartas
+ */
+export const crearDeck = (tiposDeCarta, tiposEspeciales) => {
+    if ( !tiposDeCarta || tiposDeCarta.length === 0 ) 
+        throw new Error('tiposDeCarta es obligatorio como un arreglo de string');
+
+    if ( !tiposDeCarta || tiposEspeciales.length === 0 ) 
+        throw new Error('tiposDeCarta es obligatorio como un arreglo de string');
+
+    let deck = []
+
+    for( let i = 2; i <= 10; i++ ) {
+        for( let tipo of tiposDeCarta ) {
+            deck.push( i + tipo);
+        }
+    }
+
+    for( let tipo of tiposDeCarta ) {
+        for( let esp of tiposEspeciales ) {
+            deck.push( esp + tipo);
+        }
+    }
+    // console.log( deck );
+    deck = _.shuffle( deck );
+    return deck;
+}
