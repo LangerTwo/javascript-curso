082d20c38ea9d39ec56a7911f23ae091626d320f
LangerTwo <adrangel95@gmail.com>
2024-12-16 14:16:10 -0500

    Promise hell

--- 05-advanced/src/concepts/03-promises.js
+++ 05-advanced/src/concepts/03-promises.js
@@ -5,8 +5,36 @@ import { heroes } from "../data/heros"
  * @param {HTMLDivElement} element 
  */
 export const promiseComponent = ( element ) => {
-    
-    
+    const renderHero = ( hero ) => {
+        element.innerHTML = hero.name
+    }
+
+    const renderTwoHeros = ( hero1, hero2 ) => {
+        element.innerHTML = `
+            <h3>${ hero1.name }</h3>
+            <h3>${ hero2.name }</h3>
+        `;
+    }
+
+    const renderError = ( error ) => {
+        element.innerHTML = `
+            <h3>${ error }</h3>
+        `;
+    }
+
+    const id1 = '5d86371fd55e2e2a30fe1ccb1';
+    const id2 = '5d86371fd55e2e2a30fe1cc4';
+
+    findHero( id1 )
+        .then( (hero1) => {
+
+            findHero( id2 )
+                .then( hero2 => {
+                    renderTwoHeros(hero1, hero2)
+                })
+                .catch( renderError )
+        })
+        .catch( renderError )
 }
 
 /**
@@ -15,8 +43,7 @@ export const promiseComponent = ( element ) => {
  * @returns {Promise}
  */
 const findHero = ( id ) => {
-    
-    const promise = new Promise( ( resolve, reject ) => {
+    return new Promise( ( resolve, reject ) => {
         
         const hero = heroes.find( hero => hero.id === id )
 
@@ -26,6 +53,4 @@ const findHero = ( id ) => {
         }
         reject(`Hero with id ${ id } not found`);
     });
-
-    return promise;
 }
