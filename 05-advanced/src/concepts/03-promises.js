9eefed96f4c8ea4c56c6f6e232b6809f8429cbcd
LangerTwo <adrangel95@gmail.com>
2024-12-16 13:54:59 -0500

    promesas

---
+++ 05-advanced/src/concepts/03-promises.js
@@ -0,0 +1,15 @@
+import { heroes } from "../data/heros"
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const promiseComponent = ( element ) => {
+    
+    
+}
+
+
+const findHero = ( id ) => {
+    const hero = heroes
+}
