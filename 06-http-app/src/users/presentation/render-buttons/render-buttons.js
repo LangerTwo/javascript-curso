3b633738ae43920e2906a47574a1170e7bb72371
LangerTwo <adrangel95@gmail.com>
2025-01-14 13:58:47 -0500

    render table

---
+++ 06-http-app/src/users/presentation/render-buttons/render-buttons.js
@@ -0,0 +1,29 @@
+import  usersStore  from '../../store/users-store';
+import { renderTable } from '../render-table/render-table';
+import './render-buttons.css';
+
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
+export const renderButtons = ( element ) => {
+
+    const nextButton = document.createElement('button');
+    nextButton.innerHTML = ' Next >';
+
+    const prevButton = document.createElement('button');
+    prevButton.innerHTML = '< Prev ';
+
+    const currentPageLabel = document.createElement('span');
+    currentPageLabel.id = 'current-page';
+    currentPageLabel.innerHTML = usersStore.getCurrentPage();
+
+    element.append( prevButton, currentPageLabel, nextButton );
+
+    nextButton.addEventListener('click', async () => {
+        await usersStore.loadNextPage();
+        currentPageLabel.innerHTML = usersStore.getCurrentPage();
+        renderTable( element );
+    });
+}
