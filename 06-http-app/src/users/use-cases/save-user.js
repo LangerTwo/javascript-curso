c4e104d8bf4d469e215a70ca3c121f25a41284f0
LangerTwo <adrangel95@gmail.com>
2025-01-15 15:18:48 -0500

    Caso de uso - Crear usuario

---
+++ 06-http-app/src/users/use-cases/save-user.js
@@ -0,0 +1,39 @@
+import {User} from '../models/users'
+
+/**
+ * @param {Like<User>} userLike
+*/
+export const saverUser = async ( userLike ) => {
+
+    const user = new User(userLike);
+
+    // TODO: aqui falta un mapper
+
+    if(user.id) {
+        throw 'No implementado la actualización de usuarios';
+        return;
+    }
+
+    const updatedUser = await createUser(user);
+    return updatedUser;
+}
+
+/**
+ * 
+ * @param {Like<User>} user 
+ */
+const createUser = async (user) => {
+
+    const url = `${ import.meta.env.VITE_BASE_URL }/users`;
+    const res = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+
+    const newUser = await res.json();
+    console.log({newUser});
+    return newUser;
+}
